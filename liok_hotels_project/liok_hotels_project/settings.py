38827054e5bc40cde5a1b4abb30283f1cf1f1d62
AbhijithX001 <abhijithprakash22334a@gmail.com>
2026-04-14 12:27:33 +0530

    Fix gallery contact section details visibility

--- liok_hotels_project/liok_hotels_project/settings.py
+++ liok_hotels_project/liok_hotels_project/settings.py
@@ -1,15 +1,14 @@
-import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY
-SECRET_KEY = os.environ.get("SECRET_KEY", "change-this-in-production")
+SECRET_KEY = "django-insecure-local-dev-key"
 
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # APPLICATIONS
@@ -21,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
+
     'liok_hotels_app',
 ]
 
@@ -30,7 +30,10 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+
+    # Keep CSRF enabled (important)
     'django.middleware.csrf.CsrfViewMiddleware',
+
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -46,13 +49,15 @@ ROOT_URLCONF = 'liok_hotels_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],  # make sure folder exists
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                # Make sure this file exists
                 'liok_hotels_app.context_processors.global_properties',
             ],
         },
@@ -63,7 +68,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'liok_hotels_project.wsgi.application'
 
 
-# DATABASE
+# DATABASE (good for local)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -83,16 +88,22 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # INTERNATIONALIZATION
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'   # better for you locally
 USE_I18N = True
 USE_TZ = True
 
 
-# STATIC & MEDIA
+# STATIC FILES
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",   # make sure folder exists
+]
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+
+# MEDIA FILES
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
@@ -100,13 +111,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# EMAIL (safe defaults)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.example.com")
-EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
-EMAIL_USE_TLS = True
+# EMAIL (disable errors locally)
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# This prints emails in terminal instead of sending
 
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
 
-DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "no-reply@example.com")
+# reCAPTCHA (disable locally)
+RECAPTCHA_SITE_KEY = ""
+RECAPTCHA_SECRET_KEY = ""
